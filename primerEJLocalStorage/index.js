111c448f88978267cbbb3103e918a05990cb8359
Enrique Velez <enriquevelezaguilar@gmail.com>
2024-01-15 20:20:20 +0100

    Ejercicios cookies y localstorage

--- primerEJLocalStorage/index.js
+++ primerEJLocalStorage/index.js
@@ -5,9 +5,13 @@ let nombre = document.getElementById("entername").value;
 localStorage.setItem("nombre", nombre);
 nombreparasaludo=localStorage.getItem("nombre");
 saludo.innerHTML="Welcome to our website "+nombreparasaludo;
+let pontunombre=document.getElementsByClassName("remember")[0];
+pontunombre.style.display="none";
 }
 
 function resetear(){
     saludo.innerHTML="Welcome to our website";
     localStorage.removeItem("nombre");
+    let pontunombre=document.getElementsByClassName("remember")[0];
+    pontunombre.style.display="block";
 }
